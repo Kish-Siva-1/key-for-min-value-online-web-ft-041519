e9d9eac67a82b20d9829243a8e7a415a941da3e2
Kish Siva <rodes88@msn.com>
2019-04-17 18:24:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |element|
     
     if element <= min_num
-      min_num = num
+      min_num = element
     else
       min_num = min_num
     end
